31625dd8ab109708e4dd8045f94931be38cd3018
Abdelkarim Douadjia <abdelkarim.douadjia@gmail.com>
2023-08-25 19:24:22 +0100

    Update op_ration_stack_2.c

--- op_ration_stack_2.c
+++ op_ration_stack_2.c
@@ -7,16 +7,18 @@
  */
 void mul_nod_mo(stack_t **pile, unsigned int lin_numbr)
 {
-    int som;
+	int som;
 
-    if ((*pile)->next == NULL || *pile == NULL || pile == NULL)
-        mor_e_er_mo(22, lin_numbr, "mul");
+	if ((*pile)->next == NULL || *pile == NULL || pile == NULL)
+		mor_e_er_mo(22, lin_numbr, "mul");
 
-    (*pile) = (*pile)->next;
-    som = (*pile)->n * (*pile)->prev->n;
-    (*pile)->n = som;
-    free((*pile)->prev);
-    (*pile)->prev = NULL;
+	(*pile) = (*pile)->next;
+	som = (*pile)->n * (*pile)->prev->n;
+	(*pile)->n = som;
+
+	free((*pile)->prev);
+
+	(*pile)->prev = NULL;
 }
 
 /**
@@ -26,19 +28,18 @@ void mul_nod_mo(stack_t **pile, unsigned int lin_numbr)
  */
 void mod_nod_mo(stack_t **pile, unsigned int ln_numbr)
 {
-    int some;
+	int some;
 
-    if (*pile == NULL || pile == NULL || (*pile)->next == NULL)
+	if (*pile == NULL || pile == NULL || (*pile)->next == NULL)
+		mor_e_er_mo(22, ln_numbr, "mod");
 
-        mor_e_er_mo(22, ln_numbr, "mod");
+	if ((*pile)->n == 0)
+		mor_e_er_mo(23, ln_numbr);
+	(*pile) = (*pile)->next;
+	some = (*pile)->n % (*pile)->prev->n;
+	(*pile)->n = some;
 
-    if ((*pile)->n == 0)
-        mor_e_er_mo(23, ln_numbr);
-    (*pile) = (*pile)->next;
-    some = (*pile)->n % (*pile)->prev->n;
-    (*pile)->n = some;
+	free((*pile)->prev);
 
-    free((*pile)->prev);
-
-    (*pile)->prev = NULL;
+	(*pile)->prev = NULL;
 }
